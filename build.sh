ca02afd89db761faa3bb19cc41d983842d374462
4bitFox <33175205+4bitFox@users.noreply.github.com>
2024-09-02 00:10:43 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,11 +13,25 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+#rpm-ostree install screen
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+#systemctl enable podman.socket
+
+
+
+
+### ZFS based on https://github.com/0ranki/silverblue-zfs/tree/main
+rpm-ostree override replace /tmp/rpms/kernel/*.rpm /tmp/rpms/zfs/*.rpm
+# Auto-load ZFS module
+depmod -a "$(rpm -qa kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')" && \
+echo "zfs" > /etc/modules-load.d/zfs.conf && \
+# we don't want any files on /var
+rm -rf /var/lib/pcp
+
+## Just in case, according to https://openzfs.github.io/openzfs-docs/Getting%20Started/Fedora/index.html#installation
+echo 'zfs' > /etc/dnf/protected.d/zfs.conf
